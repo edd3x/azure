b5f793ed9cb60671705f542d3ccd53306955c5e1
Ed <30408140+edd3x@users.noreply.github.com>
2022-10-02 21:07:42 +0100

    Add files via upload

---
+++ get_config_file.py
@@ -0,0 +1,13 @@
+
+from azure.storage.blob import BlobServiceClient
+
+print('Downloading xcube config file.....')
+connection_string = "DefaultEndpointsProtocol=https;AccountName=stuos4epdata01;AccountKey=lg780d4d9tVApfzYdUVFTUNtWHm4T+zWqIvf3KXVHs30JDrH5rwBUlZNV/XxPWevlDa2XS7Cz9nUg228/aKTVA==;EndpointSuffix=core.windows.net"
+# container_client = ContainerClient.from_connection_string(conn_str=connection_string, container_name='config-file')
+blob_service_client_instance = BlobServiceClient.from_connection_string(conn_str=connection_string)
+blob_client_instance = blob_service_client_instance.get_blob_client('config-file', 'xcube_config.yml', snapshot=None)
+with open('/tmp/xcube_config.yml', "wb") as my_blob:
+    blob_data = blob_client_instance.download_blob()
+    blob_data.readinto(my_blob)
+
+print('Config file downloaded succesfully...')
